e49f6b0a8b815668c61fc51ffef297794ff19c41
21pages <sunboeasy@gmail.com>
2025-03-20 10:58:25 +0800

    compile_arch_common reuse custom_res.sh
    Signed-off-by: 21pages <sunboeasy@gmail.com>

--- build.py
+++ build.py
@@ -410,17 +410,7 @@ def compile_arch_common(client_type):
     mv flutter/build/linux/{arch2}/release/bundle/rustdesk flutter/build/linux/{arch2}/release/bundle/{app_name}
 
     # handle res
-    sed -i 's/..\\/res\\/scalable.svg/..\\/res\\/128x128@2x.png/g' flatpak/rustdesk.json
-    sed -i 's/*.desktop",/*.desktop","install -Dm644 128x128@2x.png \\/app\\/share\\/icons\\/hicolor\\/256x256\\/apps\\/com.rustdesk.RustDesk.png"/g' flatpak/rustdesk.json
-    for p in res appimage flatpak; do
-        find $p -type f -exec sed -i '/rustdesk.svg/d' {{}} \\;
-        find $p -type f -exec sed -i '/scalable.svg/d' {{}} \\;
-        find $p -type f -exec sed -i 's/RustDesk/{app_name0}/g' {{}} \\;
-        find $p -type f -exec sed -i 's/rustdesk/{app_name}/g' {{}} \\;
-    done
-    mv res/rustdesk.service res/{app_name}.service
-    mv res/rustdesk.desktop res/{app_name}.desktop
-    mv res/rustdesk-link.desktop res/{app_name}-link.desktop
+    ./github/scripts/custom_res.sh {app_name0} {app_name}
     """
     )
 
